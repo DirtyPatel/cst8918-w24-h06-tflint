b2f46e3c869d2ce75520892a1477ffa10ade8263
DirtyPatel <pate0323@algonquincollege.com>
2026-03-17 13:22:33 -0400

    fixed last two warning and the added the labelprefix nd subscriptionID

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 # Define config variables
-variable "labelPrefix" {
+variable "labelprefix" {
   type        = string
   description = "Your college username. This will form the beginning of various resource names."
 }
@@ -7,6 +7,7 @@ variable "labelPrefix" {
 variable "region" {
   type    = string
   default = "westus3"
+  description = " The region were the resources would be deployed."
 }
 
 variable "admin_username" {
